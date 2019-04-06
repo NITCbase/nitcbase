7de77bc46dbf30ce3bd720f388b2119b6d7ece58
rohithv <rohith.vishnu333@gmail.com>
2019-04-07 01:04:24 +0530

    errors.h file added

---
+++ define/errors.h
@@ -0,0 +1,11 @@
+#define SUCCESS 0
+#define FAILURE -1
+#define E_OUTOFBOUND -2
+#define E_FREESLOT -3
+#define E_NOINDEX -4
+#define E_DISKFULL -5
+#define E_INVALIDBLOCK -6
+#define E_RELNOTEXIST -7
+#define E_RELEXIST -8
+#define E_ATTRNOTEXIST -9
+#define E_ATTREXIST -10
