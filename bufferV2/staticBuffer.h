9552c3472003cf6b1107e5c066aeb24a226dfbff
Visakh S <visakh1998@gmail.com>
2019-11-28 15:29:07 +0530

    Update staticBuffer.h

--- bufferV2/staticBuffer.h
+++ bufferV2/staticBuffer.h
@@ -1,2 +1,31 @@
 #include "../define/constants.h"
 #include <cstdint>
+
+struct BufferMetaInfo {
+	bool free;
+	bool dirty;
+	int blockNum;
+	int timeStamp
+};
+
+class StaticBuffer {
+				
+friend class BlockBuffer;
+
+private:
+	//fields
+	static unsigned char blocks[32][BLOCK_SIZE];
+	static struct BufferMetaInfo metainfo[32];
+	static unsigned char blockAllocMap[DISK_BLOCKS];
+
+	//methods
+	static int getFreeBuffer(int blockNum);
+	static int getBufferNum(int blockNum);
+	
+public:
+	//methods	
+	static int getStaticBlockType(int blockNum);
+	StaticBuffer();
+	~StaticBuffer();
+	
+};
