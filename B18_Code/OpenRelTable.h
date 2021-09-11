4fd1cf3e0d50c9cf50b494a8a28fa902dece2afb
Jessiya Joy <jessiyajoy@gmail.com>
2021-09-11 14:48:21 +0530

    Basic layout for OpenRelations Class added

--- B18_Code/OpenRelTable.h
+++ B18_Code/OpenRelTable.h
@@ -6,6 +6,22 @@
 #define B18_CODE_OPENRELTABLE_H
 #include "../define/constants.h"
 
-extern char OpenRelTable[MAXOPEN][16];
+extern char OpenRelTable[MAX_OPEN][16];
+
+class OpenRelations {
+	static char OpenRelTable[MAX_OPEN][16];
+
+	int getRelationId(char relationName[16])
+	{
+	}
+
+	int openRelation(char relationName[16])
+	{
+	}
+
+	int closeRelation(int relationId)
+	{
+	}
+};
 
 #endif //B18_CODE_OPENRELTABLE_H
