f56a23ca91ba3c2dfd3c04b8cc7efb8205b0ede9
Jessiya Joy <jessiyajoy@gmail.com>
2021-09-12 15:02:31 +0530

    openRelation, closeRelation, getRelationId functions added in OpenRelations Class

--- B18_Code/OpenRelTable.h
+++ B18_Code/OpenRelTable.h
@@ -4,23 +4,75 @@
 
 #ifndef B18_CODE_OPENRELTABLE_H
 #define B18_CODE_OPENRELTABLE_H
-#include "../define/constants.h"
 
-extern char OpenRelTable[MAX_OPEN][16];
+#include "../define/constants.h"
+#include "block_access.h"
+
+//extern char OpenRelTable[MAX_OPEN][16];
 
 class OpenRelations {
-	static char OpenRelTable[MAX_OPEN][16];
+	static char OpenRelTable[MAX_OPEN][ATTR_SIZE];
 
-	int getRelationId(char relationName[16])
-	{
+public:
+
+	static int getRelationId(char relationName[ATTR_SIZE]) {
+		for (int i = 0; i < MAX_OPEN; i++)
+			if (strcmp(OpenRelTable[i], relationName) == 0)
+				return i;
+		return E_RELNOTOPEN;
 	}
 
-	int openRelation(char relationName[16])
-	{
+	static int openRelation(char relationName[ATTR_SIZE]) {
+		Attribute relationCatalog[6];
+
+		/* check if relation exists
+		 *      for this check each entry in relation catalog
+		 */
+		int i;
+		for (i = 0; i < SLOTMAP_SIZE_RELCAT_ATTRCAT; i++) {
+			getRecord(relationCatalog, 4, i);
+			if (strcmp(relationCatalog[0].sval, relationName) == 0) {
+				break;
+			}
+		}
+
+		// if relation does not exist
+		if (i == SLOTMAP_SIZE_RELCAT_ATTRCAT) {
+			return E_RELNOTEXIST;
+		}
+
+		/* check if relation is already open
+		 *      if yes, return open relation id
+		 *  otherwise search for a free slot in open relation table
+		 */
+		for (i = 0; i < MAX_OPEN; i++) {
+			if (strcmp(relationName, OpenRelTable[i]) == 0) {
+				return i;
+			}
+		}
+
+		for (i = 0; i < MAX_OPEN; i++) {
+			if (strcmp(OpenRelTable[i], "NULL") == 0) {
+				strcpy(OpenRelTable[i], relationName);
+				return i;
+			}
+		}
+
+		// if open relation table is already full
+		if (i == MAX_OPEN) {
+			return E_CACHEFULL;
+		}
 	}
 
-	int closeRelation(int relationId)
-	{
+	static int closeRelation(int relationId) {
+		if (relationId < 0 || relationId >= MAX_OPEN) {
+			return E_OUTOFBOUND;
+		}
+		if (strcmp(OpenRelTable[relationId], "NULL") == 0) {
+			return E_RELNOTOPEN;
+		}
+		strcpy(OpenRelTable[relationId], "NULL");
+		return 0;
 	}
 };
 
