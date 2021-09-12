e8b38b695981b565ad4ea60c0e4f9040e91783bd
jessiyaj <jessiya_b180462cs@nitc.ac.in>
2021-09-12 19:31:23 +0530

    enum declarations for AttributeType and AttributeType

---
+++ B18_Code/define/enum_constants.h
@@ -0,0 +1,29 @@
+//
+// Created by Jessiya Joy on 11/09/21.
+//
+
+#ifndef B18_CODE_ENUM_CONSTANTS_H
+#define B18_CODE_ENUM_CONSTANTS_H
+
+enum AttributeType {
+	STRING,
+	NUMBER
+};
+
+enum ConditionalOperators {
+	EQ, // =
+	LE, // <=
+	LT, // <
+	GE, // >=
+	GT, // >
+	NE // !=
+}
+#endif //B18_CODE_ENUM_CONSTANTS_H
+
+/* TODO :
+ * - Return types
+ * - Block types
+ * - Block Header fields
+ * - AttrCat entries
+ * - RelCat entries
+ */
