625092e4125aca04f45a7252e965c58f68db222b
jessiyaj <jessiya_b180462cs@nitc.ac.in>
2021-09-12 22:09:40 +0530

    Adding OpenRelTable header file

--- B18_Code/OpenRelTable.h
+++ B18_Code/OpenRelTable.h
@@ -14,109 +14,19 @@ class OpenRelations {
 	static char OpenRelTable[MAX_OPEN][ATTR_SIZE];
 
 public:
-	static void initializeOpenRelationTable() {
-		for (int i = 0; i < MAX_OPEN; i++) {
-			if (i == RELCAT_RELID)
-				strcpy(OpenRelTable[i], "RELATIONCAT");
-			else if (i == ATTRCAT_RELID)
-				strcpy(OpenRelTable[i], "ATTRIBUTECAT");
-			else
-				strcpy(OpenRelTable[i], "NULL");
-		}
-	}
+	static void initializeOpenRelationTable();
 
-	static int getRelationId(char relationName[ATTR_SIZE]) {
-		for (int i = 0; i < MAX_OPEN; i++)
-			if (strcmp(OpenRelTable[i], relationName) == 0)
-				return i;
-		return E_RELNOTOPEN;
-	}
+	static int getRelationId(char relationName[ATTR_SIZE]);
 
-	static int getRelationName(int relationId, char relationName[ATTR_SIZE]) {
-		if (relationId < 0 || relationId >= MAX_OPEN) {
-			return E_OUTOFBOUND;
-		}
-		strcpy(relationName, OpenRelTable[relationId]);
-		return 0;
-	}
+	static int getRelationName(int relationId, char relationName[ATTR_SIZE]);
 
-	static int openRelation(char relationName[ATTR_SIZE]) {
-		Attribute relationCatalog[6];
+	static int openRelation(char relationName[ATTR_SIZE]);
 
-		/* check if relation exists
-		 *      for this check each entry in relation catalog
-		 */
-		int i;
-		for (i = 0; i < SLOTMAP_SIZE_RELCAT_ATTRCAT; i++) {
-			getRecord(relationCatalog, 4, i);
-			if (strcmp(relationCatalog[0].sval, relationName) == 0) {
-				break;
-			}
-		}
+	static int closeRelation(int relationId);
 
-		// if relation does not exist
-		if (i == SLOTMAP_SIZE_RELCAT_ATTRCAT) {
-			return E_RELNOTEXIST;
-		}
+	static int checkIfRelationOpen(char relationName[ATTR_SIZE]);
 
-		/* check if relation is already open
-		 *      if yes, return open relation id
-		 *  otherwise search for a free slot in open relation table
-		 */
-		for (i = 0; i < MAX_OPEN; i++) {
-			if (strcmp(relationName, OpenRelTable[i]) == 0) {
-				return i;
-			}
-		}
-
-		for (i = 0; i < MAX_OPEN; i++) {
-			if (strcmp(OpenRelTable[i], "NULL") == 0) {
-				strcpy(OpenRelTable[i], relationName);
-				return i;
-			}
-		}
-
-		// if open relation table is already full
-		if (i == MAX_OPEN) {
-			return E_CACHEFULL;
-		}
-	}
-
-	static int closeRelation(int relationId) {
-		if (relationId < 0 || relationId >= MAX_OPEN) {
-			return E_OUTOFBOUND;
-		}
-		if (strcmp(OpenRelTable[relationId], "NULL") == 0) {
-			return E_RELNOTOPEN;
-		}
-		strcpy(OpenRelTable[relationId], "NULL");
-		return 0;
-	}
-
-	/*
-	 * If yes, return 1
-	 * else, 0
-	 */
-	static int checkIfRelationOpen(char relationName[ATTR_SIZE]) {
-		for (auto relationIterator: OpenRelTable) {
-			if (strcmp(relationIterator, relationName) == 0) {
-				return 1;
-			}
-		}
-		return 0;
-	}
-
-	static int checkIfRelationOpen(int relationId) {
-		if (relationId < 0 || relationId >= MAX_OPEN) {
-			return E_OUTOFBOUND;
-		}
-		if (strcmp(OpenRelTable[relationId], "NULL") == 0) {
-			return 0;
-		}
-		else {
-			return 1;
-		}
-	}
+	static int checkIfRelationOpen(int relationId);
 };
 
 #endif //B18_CODE_OPENRELTABLE_H
