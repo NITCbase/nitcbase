fcf073604c73a3e2c150742abe597067c24c9e04
gokulsreekumar <gokulsreekumar2@gmail.com>
2021-10-31 13:19:53 +0530

    Disk Class Read and Write code added

--- B18_Code/Frontend_Interface/Disk.cpp
+++ B18_Code/Frontend_Interface/Disk.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Disk.h"
+
+int Disk::readBlock(unsigned char *block, int blockNum) {
+    FILE *disk = fopen(DISK_PATH, "rb");
+    const int offset = blockNum * BLOCK_SIZE;
+    fseek(disk, offset, SEEK_SET);
+    fread(block, BLOCK_SIZE, 1, disk);
+    fclose(disk);
+}
+
+int Disk::writeBlock(unsigned char *block, int blockNum) {
+    FILE *disk = fopen(DISK_PATH, "wb");
+    int offset = blockNum * BLOCK_SIZE;
+    fseek(disk, offset, SEEK_SET);
+    fwrite(block, BLOCK_SIZE, 1, disk);
+    fclose(disk);
+}
