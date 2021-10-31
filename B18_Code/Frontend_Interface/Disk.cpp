7948213f142eef35e121257168e3d743126307d2
gokulsreekumar <gokulsreekumar2@gmail.com>
2021-10-31 13:24:36 +0530

    Read block error returns added

--- B18_Code/Frontend_Interface/Disk.cpp
+++ B18_Code/Frontend_Interface/Disk.cpp
@@ -2,14 +2,21 @@
 // Created by Gokul Sreekumar on 31/10/21.
 //
 
+#include <iostream>
+#include "../define/constants.h"
+#include "../define/errors.h"
 #include "Disk.h"
 
 int Disk::readBlock(unsigned char *block, int blockNum) {
     FILE *disk = fopen(DISK_PATH, "rb");
+    if (blockNum < 0 || blockNum > 8191) {
+        return E_OUTOFBOUND;
+    }
     const int offset = blockNum * BLOCK_SIZE;
     fseek(disk, offset, SEEK_SET);
     fread(block, BLOCK_SIZE, 1, disk);
     fclose(disk);
+    return SUCCESS;
 }
 
 int Disk::writeBlock(unsigned char *block, int blockNum) {
