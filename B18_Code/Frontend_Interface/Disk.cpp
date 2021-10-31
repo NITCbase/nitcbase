a1bb3af4afa5241ab1f1f746595031b07f3067a5
gokulsreekumar <gokulsreekumar2@gmail.com>
2021-10-31 12:42:35 +0530

    Disk Class File added and CMakeLists updated
    - Disk class file and its header

---
+++ B18_Code/Frontend_Interface/Disk.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gokul Sreekumar on 31/10/21.
+//
+
+#include "Disk.h"
---
+++ B18_Code/Frontend_Interface/Disk.h
@@ -0,0 +1,8 @@
+//
+// Created by Gokul Sreekumar on 31/10/21.
+//
+
+#ifndef FRONTEND_INTERFACE_H
+#define FRONTEND_INTERFACE_H
+
+#endif //FRONTEND_INTERFACE_H
