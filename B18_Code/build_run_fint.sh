8c7ebfd77e6f7da9446b9cc883258d336be5ee41
gokulsreekumar <gokulsreekumar2@gmail.com>
2021-12-02 13:06:51 +0530

    Containerize Front end and Disk Impl
    - Two new files run_fint.sh and build_run_fint.sh
    - One new Docker file -> DockerFile_1

---
+++ B18_Code/build_run_fint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#build image with tag
+
+echo Enter tag
+
+# shellcheck disable=SC2162
+read tag
+
+echo Building docker image frontendinterface:"$tag"
+
+docker build . -f Dockerfile_1 -t frontendinterface:"$tag"
+
+echo Running docker container from image frontendinterface:"$tag"
+
+docker run --rm -it -v "$HOME"/Developer/nitcbase/B18_Code/Files:/opt/B18_Code/Files -v "$HOME"/Developer/nitcbase/B18_Code/Disk:/opt/B18_Code/Disk frontendinterface:"$tag"
