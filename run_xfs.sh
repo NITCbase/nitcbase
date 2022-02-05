e430bf628cbb8512307e47bc257f873f283403cd
gokulsreekumar <gokulsreekumar2@gmail.com>
2022-02-05 15:27:03 +0530

    Run files for XFS and Frontend Docker Containers modified
    - Now the run can be done from any directory
    - Disk and File paths wrongly names, fixed
    - real path used

--- run_xfs.sh
+++ run_xfs.sh
@@ -5,8 +5,7 @@ echo Running docker container from image xfs
 # shellcheck disable=SC2006
 project_root=`realpath .`
 
-disk_volume_path="$project_root/Files:/opt/NITCbase/Disk"
-
-files_volume_path="$project_root/Disk:/opt/NITCbase/Files"
+disk_volume_path="$project_root/Disk:/opt/NITCbase/Disk"
+files_volume_path="$project_root/Files:/opt/NITCbase/Files"
 
 docker run --rm -it -v "$files_volume_path" -v "$disk_volume_path" xfs
